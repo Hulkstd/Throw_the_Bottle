e934bb67d6af6316f9b89bf51b62a9d5d15c08ac
Nanachi <36787932+Jinsab@users.noreply.github.com>
2019-03-11 21:42:43 +0900

    씬 추가, UI 기본창 추가
    앞으로 추가할 부분 아직 많음. 옵션창 잘 타협하면 될 것 같음.

---
+++ Assets/Script/OptionTable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionTable : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("Option");
+    }
+}
