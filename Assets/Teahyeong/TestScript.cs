e570681c70f28f047a74ff38906eb7ef9bfc556d
c0cac01a <joobs1127@gmail.com>
2019-03-12 13:14:54 +0900

    AA

--- Assets/Teahyeong/TestScript.cs
+++ Assets/Teahyeong/TestScript.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     public Rigidbody2D Rig;
+    bool isGet;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGet = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKey(KeyCode.Space) && !isGet)
         {
-            Rig.AddForceAtPosition(Vector2.one * 25, Vector2.down * 3, ForceMode2D.Impulse);
-            Rig.AddForce(Vector2.right * 100, ForceMode2D.Impulse);
+            Rig.AddForceAtPosition(Vector2.one * 80, Vector2.down * 3, ForceMode2D.Impulse);
+            Rig.AddForce(Vector2.right * 25, ForceMode2D.Impulse);
+            isGet = true;
         }
     }
 }
