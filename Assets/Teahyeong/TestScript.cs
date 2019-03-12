d8f17fd71014ee7efe9165d04ddefadc5c480fb2
Nanachi <36787932+Jinsab@users.noreply.github.com>
2019-03-12 19:28:59 +0900

    Somebody help you!
    도와줘ㅋ

---
+++ Assets/Teahyeong/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public Rigidbody2D Rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rig.AddForceAtPosition(Vector2.one * 5, Vector2.down * 3,ForceMode2D.Impulse);
+        Rig.AddForce(Vector2.right * 20, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
