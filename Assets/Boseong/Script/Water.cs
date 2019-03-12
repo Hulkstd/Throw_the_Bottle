567b496f25a45e4b2fe6f5bb3f7b454f11bf88c3
c0cac01a <joobs1127@gmail.com>
2019-03-12 19:50:49 +0900

    waterandbottle

---
+++ Assets/Boseong/Script/Water.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            gameObject.SetActive(false);
+            Debug.Log(gameObject.name);
+        }
+    }
+
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestScript : MonoBehaviour
+{
+    
+    public Rigidbody2D Rig;
+    bool isGet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGet = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && !isGet)
+        {
+            Rig.AddForceAtPosition(Vector2.one * 120, Vector2.down * 3, ForceMode2D.Impulse);
+            Rig.AddForce(Vector2.right * 200, ForceMode2D.Impulse);
+            isGet = true;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+    }
+}
