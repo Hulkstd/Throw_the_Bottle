a42aae34a4a0bcccab0ce613ca6e8c62219430d8
hulkSTD <hulk1257@gmail,com>
2019-03-13 19:07:11 +0900

    Add BGM and Add SceneChanger

--- Assets/Script/Option/OptionTable.cs
+++ Assets/Script/Option/OptionTable.cs
@@ -4,14 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class OptionTable : MonoBehaviour
 {
+    public SceneChanger sceneChanger;
+
     /* 이름 : OptionTable
      * 역할 : 메인 메뉴에서 옵션 메뉴로 이동할 때 사용
      * 최종작업 날짜 : 2019-03-11 오후 9:46
      * 변경사항 : X
-     */ 
+     */
+
+    private void Start()
+    {
+        sceneChanger = SceneChanger.GetSceneChanger;
+    }
 
     public void ChangeGameScene()
     {
-        SceneManager.LoadScene("Option");
+        sceneChanger.ChangeScene("Option");
     }
 }
