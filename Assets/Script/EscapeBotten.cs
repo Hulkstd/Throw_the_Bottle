ce8a420dcff9f27f20ed25db4e297d7180bb4c18
c0cac01a <joobs1127@gmail.com>
2019-04-09 22:09:19 +0900

    Add Escape Button

---
+++ Assets/Script/EscapeBotten.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeBotten : MonoBehaviour
+{
+
+    public string NowStage;
+
+    private readonly string[] StageStr = { "Menu", "ModeChoose", "Campain" };
+
+    private bool InGame;
+
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                switch (NowStage)
+                {
+                    case "Menu":
+                        Application.Quit();
+                        break;
+                    case "ModeChoose":
+                        SceneManager.LoadScene(StageStr[0]);
+                        break;
+                    case "Option":
+                        SceneManager.LoadScene(StageStr[0]);
+                        break;
+                    case "Campain":
+                        SceneManager.LoadScene(StageStr[1]);
+                        break;
+                    case "TimeAttack":
+                        if (!InGame)
+                        {
+                            SceneManager.LoadScene(StageStr[1]);
+                        }
+                        break;
+                    case "Stage":
+                        SceneManager.LoadScene(StageStr[2]);
+                        break;
+                }
+            }
+        }
+    }
+}
